3fa92b3d6e036ff8aeecf5285fb2f726fcd2ad0a
andyt1011 <andy20021011@gmail.com>
2022-08-06 19:16:33 +0800

    coin

--- lab2_git.py
+++ lab2_git.py
@@ -82,7 +82,11 @@ def toss_coin():
     """TODO: Add your code here
         Hint: Using random.randint() function.
     """
-    coin = 'Head'
+    num_1 = random.randint(0,1)
+    if num_1 == 0:
+        coin = 'Head'
+    if num_1 == 1:
+        coin = 'Tail'
     return coin
 
 
